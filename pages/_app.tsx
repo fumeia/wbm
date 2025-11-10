5fd517781e6cc28db78a34b6222554097aa1062c
fumeia <nikoberschin@gmail.com>
2025-11-10 15:39:08 +0100

    light mode v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,18 +6,20 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-      {/*
-        Umami Analytics Script
-        - Replace data-website-id with your own ID
-        - Replace src with your Umami instance URL
-      */}
-      <Script
-        async
-        defer
-        data-website-id="YOUR_UMAMI_WEBSITE_ID"
-        src="YOUR_UMAMI_URL"
-      />
+      <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
+        <Component {...pageProps} />
+        {/*
+          Umami Analytics Script
+          - Replace data-website-id with your own ID
+          - Replace src with your Umami instance URL
+        */}
+        <Script
+          async
+          defer
+          data-website-id="YOUR_UMAMI_WEBSITE_ID"
+          src="YOUR_UMAMI_URL"
+        />
+      </div>
     </ThemeProvider>
   );
 }
