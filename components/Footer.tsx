ee6675c5009b51b685500edc92a2f59b878f8a18
fumeia <nikoberschin@gmail.com>
2025-11-10 15:29:53 +0100

    changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,15 @@ const Footer = () => {
     <footer className="py-8 mt-16 border-t">
       <div className="container mx-auto text-center text-gray-600 dark:text-gray-400">
         {/* --- CUSTOMIZATION --- */}
-        <div className="flex justify-center space-x-4">
+        <div className="flex justify-center items-center space-x-4">
+          <span>Contact:</span>
           <a
-            href="https://github.com/fumeia"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="mailto:contact@nikolaus-berschin.eu"
             className="hover:text-black dark:hover:text-white"
           >
-            GitHub
+            write an E-Mail
           </a>
+          <span>|</span>
           <a
             href="https://linkedin.com/in/nikolaus-berschin"
             target="_blank"
@@ -19,6 +19,14 @@ const Footer = () => {
             className="hover:text-black dark:hover:text-white"
           >
             LinkedIn
+          </a>
+          <a
+            href="https://github.com/fumeia"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-black dark:hover:text-white"
+          >
+            GitHub
           </a>
           <a
             href="/cv.pdf"
