ca81c9a66cd81194ecef36a250071e2daa24835b
fumeia <nikoberschin@gmail.com>
2025-11-11 18:31:39 +0100

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
         <Component {...pageProps} />
         {/*
